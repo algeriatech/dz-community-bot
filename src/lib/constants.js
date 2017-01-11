6a54eac7052e3ab520ff82459576645cce962642
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2017-01-12 00:32:01 +0100

    Added message patterns and events constants

---
+++ src/lib/constants.js
@@ -0,0 +1,10 @@
+const SALUTATIONS = [
+  /^s[ea]l[ea]m.*/i,
+  /^s(alu|l)t.*/i,
+  /^(hi|hello)/i
+]
+
+
+module.exports = {
+  SALUTATIONS,
+}
---
+++ src/lib/events.js
@@ -0,0 +1,18 @@
+const MESSAGE_RECEIVED  = 'message_received'
+const USER_TEAM_JOIN    = 'team_join'
+const USER_CHANNEL_JOIN = 'user_channel_join'
+const BOT_CHANNEL_JOIN  = 'bot_channel_join'
+const MENTION           = 'mention'
+const MENTION_DIRECT    = 'direct_mention'
+const NO_MENTION        = 'ambient'
+
+
+module.exports = {
+  MESSAGE_RECEIVED,
+  USER_TEAM_JOIN,
+  USER_CHANNEL_JOIN,
+  BOT_CHANNEL_JOIN,
+  MENTION,
+  MENTION_DIRECT,
+  NO_MENTION,
+}
