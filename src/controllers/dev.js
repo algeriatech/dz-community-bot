4a8430129bddeb544b4befa5323d801a8eee4b54
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2017-01-13 13:59:38 +0100

    Separate Dev token from Prod one Use different tokens for the two versions of the bot: API_TOKEN for @iobot and API_TOKEN_DEV for @iobot-dev

--- src/controllers/dev.js
+++ src/controllers/dev.js
@@ -4,7 +4,10 @@
  * @param {string|object} msg The message to send back
  */
 const devToken = (bot, msg) => {
-  let m = `Here is a token for you. Please use it wisely :wink:\n${process.env.API_TOKEN_DEV}`
+  let m = 'Sorry! I do not have a token right now :/\nContact one of the admins'
+
+  if (process.env.API_TOKEN_DEV)
+    m = `Here is a token for you. Please use it wisely :wink:\n${process.env.API_TOKEN_DEV}`
 
   bot.reply(msg, m)
 }
