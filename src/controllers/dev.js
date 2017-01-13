8410fdce404dfc21950b6b0efab22d42bb71a088
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2017-01-13 16:07:44 +0100

    Updated the instructions to use the API_TOKEN

--- src/controllers/dev.js
+++ src/controllers/dev.js
@@ -7,7 +7,9 @@ const devToken = (bot, msg) => {
   let m = 'Sorry! I do not have a token right now :/\nContact one of the admins'
 
   if (process.env.API_TOKEN_DEV)
-    m = `Here is a token for you. Please use it wisely :wink:\n${process.env.API_TOKEN_DEV}`
+    m = `Here is a token for you, paste it in you \`.env\` file
+\`API_TOKEN = ${process.env.API_TOKEN_DEV}\`
+Please use is wisely :wink:`
 
   bot.reply(msg, m)
 }
