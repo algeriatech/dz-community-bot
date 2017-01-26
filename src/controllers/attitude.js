9552efa71e9f2637ff9ab750c64ef6c14f1a540a
Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
2017-01-26 19:38:13 +0100

    another minor changes

--- src/controllers/attitude.js
+++ src/controllers/attitude.js
@@ -1,6 +1,6 @@
 
 const El9der = (bot, msg) => {
-    bot.reply(msg, `<@${msg.user}>: 9ader rohak, ma3djbaksh el 7al atchu shui Pull Requests`)
+  bot.reply(msg, `<@${msg.user}>: 9ader rohak, ma3djbaksh el 7al atchu shui Pull Requests`)
 }
 
-module.exports = {El9der};
+module.exports = {El9der}
--- src/controllers/location.js
+++ src/controllers/location.js
@@ -3,4 +3,4 @@ const WhereAreYou = (bot, msg) => {
   bot.reply(msg, `<@${msg.user}>:  Obviously in the Cloud, but what's the Cloud anyway :wink:`)
 }
 
-module.exports = {WhereAreYou};
+module.exports = {WhereAreYou}
